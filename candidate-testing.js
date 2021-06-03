11cc482a3883654583786387ecdb7e0faf8f625f
Colten Schneider <>
2021-06-03 03:04:57 +0000

    Added questions and answers

--- candidate-testing.js
+++ candidate-testing.js
@@ -2,25 +2,43 @@ const input = require('readline-sync');
 
 // TODO 2: modify your quiz app to ask 5 questions //
 
-// TODO 1.1a: Define candidateName // 
+// 1.1a: Define candidateName // 
+
 let candidateName = "";
+
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+
+let question = "What is your name? ";
+let correctAnswer = "7";
+let candidateAnswer = "";
+let questions = 
+["Who was the first American woman in space? ",
+ "True or false: 5 kilometer == 5000 meters? ",
+ "(5 + 3)/2 * 10 = ? ",
+ "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+ "What is the minimum crew size for the ISS? "];
+let correctAnswers = 
+[ 
+  "Sally Ride",
+  "true",
+  "40",
+  "Trajectory",
+  "3"
+];
+
+let candidateAnswers = [];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  candidateName = input.question("enter name: ")
+  candidateName = input.question(question);
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+  for (i=0; i < questions.length; i++) { 
+  candidateAnswers[i] = input.question(questions[i]);
+  }
 
 }
 
@@ -28,17 +46,26 @@ function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
-
-  let grade;
+  let grade = 100;
   
+  for (i = 0; i < correctAnswers.length; i++) {
+    if (candidateAnswers[i] != correctAnswers[i]) {
+      grade = grade - 20;
+    }
+  }
 
-  return grade;
+  if (grade >= 80) {
+      console.log("Congratulations, you passed with a: " + grade);
+  }
+  else {
+    console.log("You did not pass. You scored a: " + grade);
+  }
 }
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  console.log("Hello " + candidateName)
+  console.log("Hello " + candidateName);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
@@ -56,3 +83,5 @@ module.exports = {
   gradeQuiz: gradeQuiz,
   runProgram: runProgram
 };
+
+
